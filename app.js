2a5970da249179b3368d97b2c93de1a3be32b1ca
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-10 16:00:12 +0530

    primitive datatypes

---
+++ app.js
@@ -0,0 +1,23 @@
+// TO PRINT SOMETHING IN JS
+// console.log('Hello World!')
+// console.warn('hello warning')
+// console.error('hello error')
+
+// DATATYPES
+// string
+console.log("hello")
+console.log(typeof "hello")
+console.log('D')
+console.log(typeof "D")
+
+// numbers
+console.log(45)
+console.log(typeof 45)
+console.log(-675432356778)
+console.log(typeof -675432356778 )
+
+// boolean
+console.log(true)
+console.log(typeof true)
+console.log(false)
+console.log(typeof false)
