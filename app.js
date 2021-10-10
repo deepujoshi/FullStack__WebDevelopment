bf12117dd0bee5d47bd5d7f001255108e8184e4b
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-10 16:43:07 +0530

    varibles

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-// varibles
-// var age = 19
+// NON PRIMITIVE DATATYPES
+
+// 1.array
+
+var arr =[1,2,3,4,] //to declare an array
+console.log(  typeof arr)
+
+// ARRAY METHODS
+// Length
+
+var arr =[1,2,3,4]
+console.log(arr)
+console.log(arr.length)
+
+// push
+var arr =[1,2,3,4]
+arr.push(5)
+console.log(arr)
+console.log(arr.length)
+
+
+// we can reassign a variable value when we use "var" keyword
+// if we dont want to reassign a value we use "const" keyword
+// const age = 19
+// age = 21
 // console.log(age)
 
-var age 
-console.log(age) // Undefined
-// var age 
-// console.log(Name)// ReferenceError
-// var age = null
-// console.log(age) // null
-
-
-var age 
-console.log( typeof age) 
-
-var age = null
-console.log(typeof age)
