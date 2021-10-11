0873e66481bdb694b2fcf0b3741e61a191f8426d
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-11 16:12:41 +0530

    DOM API selection methods

--- app.js
+++ app.js
@@ -1,57 +1,14 @@
-// OPERATORS
-var a = 2
-var b = 10
+var heading = document.getElementById("heading")
+var para = document.getElementsByClassName("para")
+var tag = document.getElementsByTagName("a")
+var query = document.querySelector("#heading")
+var query1 = document.querySelector(".para")
+console.log(para.length)
+console.log(para[0])
+console.log(para[1])
+console.log(heading)
+console.log(tag)
+console.log(query)
+console.log(query1)
 
-// Arithmetic operators
-
-console.log('a + b =', a + b)
-console.log('b - a =', b - a)
-console.log('b / a =', b / a)
-console.log('b * a =', b * a)
-console.log('b ** a =', b ** a)
-console.log('b % a =', b % a)
-
-
-// Assignment Operators
-console.log('b ', b)
-b += 10
-console.log('b+=10 ', b)
-b++
-console.log('b++ ', b)
-b -= 10
-console.log('b-=10 ', b)
-b--
-console.log('b-- ', b)
-
-// post & 
-// var c = 10
-// console.warn(c++)
-// console.log(c)
-// console.warn(++c)
-
-// Comparison operators
-
-var d = 10
-var e = 2
-var f = '10'
-
-console.log(d > e)
-console.log(d < e)
-console.log(d >= e)
-console.log(d <= e)
-console.log(d == e)
-
-console.warn(d == f)
-console.warn(d === f)
-
-// Logical operators
-
-console.log('true && true ', true && true)
-console.log('true && false', true && false)
-console.log('false && true', false && true)
-console.log('false && false', false && false)
-
-console.log('true || true ', true || true)
-console.log('true || false', true || false)
-console.log('false || true', false || true)
-console.log('false || false', false || false)
+// para.push("deepu")
