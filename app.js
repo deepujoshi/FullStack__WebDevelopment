c924cf1988cc8c1a5bb2747cc60790d09fa524aa
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-12 16:41:04 +0530

    Function Expression(Type-2)

--- app.js
+++ app.js
@@ -1,39 +1,26 @@
 // FUNCTIONS
+// 2. "FUNCTION EXPRESSION : its just assigning your function to a variable
 
-// just fuctions
-// fuction greet(){
-// console.log('hello')
+         //using fuction keyword
+         
+// function add(a=0,b=0){
+//   var sum = a+b 
+//   return sum
 // }
-// greet ("Deepthi")
+// console.log(add(10,20))
 
 
-// fuctions with paraments
-// function greet(Name, age){
-//   console.log(Name, age)
-//   console.log('hello')
-// }
-// greet('deepthi', 19)
-
-// Passing Default paramenters
-// function greet (Name='john' , age=99){
-//   console.log(Name, age)
-//   console.log(Name + " your age is  " + age)
-// } 
-// greet('deepthi', 21)
-
-
-// function sum(a , b){
-//   console.log(a + b)
-// }
-// sum(10, 20)
-// sum(10) // output will be " NaN(Not a Number) "
-
-function add(a=0,b=0){
-  var sum = a+b 
-  return sum
+      //  using fuction expression
+      
+var  add = function(a=0,b=0){
+var sum = a+b 
+return sum
 }
-// console.log(sum) // retuns an error cuz function variables are limited to the function scope only
-                    //  U can't get the output is used like this 
-// console.log(add(10,20)) OR
-var result = add(10,20)
-console.log(result)
+console.log(add(10,20)) 
+//  
+
+      //  using fuction expression examples
+// var greet = function(name='john' , age='99'){
+//   console.log(name,age)
+// }
+// greet('deepthi', 21)
