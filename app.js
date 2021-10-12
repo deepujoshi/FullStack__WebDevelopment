61771ce723afb904e8a337d7c69d3f810f07bec7
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-12 16:50:11 +0530

    Arrow Functions

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-// FUNCTIONS
-// 2. "FUNCTION EXPRESSION : its just assigning your function to a variable
+// ARROW FUNCTIONS
+// Its just replacing "function" keyword with "=>"
 
-         //using fuction keyword
-         
-// function add(a=0,b=0){
-//   var sum = a+b 
-//   return sum
-// }
-// console.log(add(10,20))
-
-
-      //  using fuction expression
-      
-var  add = function(a=0,b=0){
-var sum = a+b 
-return sum
+var add = (a=0,b=0) => {
+  var sum = a + b
+  return sum
 }
-console.log(add(10,20)) 
-//  
-
-      //  using fuction expression examples
-// var greet = function(name='john' , age='99'){
-//   console.log(name,age)
-// }
-// greet('deepthi', 21)
+console.log(add(10,20))
