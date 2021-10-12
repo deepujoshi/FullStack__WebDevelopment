8a0698b1c53acd19344d9888d12478e60d96a0e2
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-12 17:09:55 +0530

    Changes in Arrow Function

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 // ARROW FUNCTIONS
 // Its just replacing "function" keyword with "=>"
 
+// var add = (a=0,b=0) => {
+//   var sum = a + b
+//   return sum
+// }
+// console.log(add(10,20))
+
+// can use like this
 var add = (a=0,b=0) => {
-  var sum = a + b
-  return sum
+  return a + b
 }
 console.log(add(10,20))
+
+// can be used like this also
+var add = (a=0,b=0) => a + b
+console.log(add(10,20))
+
+
+// Arrow Function example
+// const greet = (Name) => {
+//   return 'hi ' + Name
+// }
+// console.log(greet('Deepthi'))
+
+        // OR
+// if we just have one parameter USE LIKE THIS
+
+const greet = Name =>'hi ' + Name
+
+console.log(greet('Deepu'))
+
+      
