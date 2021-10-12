f867dfeff2e6e96534f28af0eafdab6b3b724247
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-12 16:22:25 +0530

    Function Keyword(type-1)

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-var list = document.querySelector('ul')
-console.log(list)
+// FUNCTIONS
 
-var arr =['go to gym','cook oatmeal' , 'eat']
+// just fuctions
+// fuction greet(){
+// console.log('hello')
+// }
+// greet ("Deepthi")
 
-for(var i=0; i< arr.length; i++){
-  var element = document.createElement('li')
-  var node = document.createTextNode(arr[i])
-element.appendChild(node)
-list.appendChild(element)
+
+// fuctions with paraments
+// function greet(Name, age){
+//   console.log(Name, age)
+//   console.log('hello')
+// }
+// greet('deepthi', 19)
+
+// Passing Default paramenters
+// function greet (Name='john' , age=99){
+//   console.log(Name, age)
+//   console.log(Name + " your age is  " + age)
+// } 
+// greet('deepthi', 21)
+
+
+// function sum(a , b){
+//   console.log(a + b)
+// }
+// sum(10, 20)
+// sum(10) // output will be " NaN(Not a Number) "
+
+function add(a=0,b=0){
+  var sum = a+b 
+  return sum
 }
+// console.log(sum) // retuns an error cuz function variables are limited to the function scope only
+                    //  U can't get the output is used like this 
+// console.log(add(10,20)) OR
+var result = add(10,20)
+console.log(result)
