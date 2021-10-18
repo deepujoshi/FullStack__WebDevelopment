244a0abe1be66fb1978d6990597c9824195dda6c
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-18 19:11:28 +0530

    VAR v/s LET

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-    // ARRAY METHODS
+// 'let' and 'var' 
+     
+// let is a block scope
+// var is a global scope
 
-// var arr=[1,2,3,4,5]
+// if (true){
+//     let Name = 'Ferrari'
+//     console.log(Name)
+//     }
 
-// arr.push(6)
-// arr.unshift(6)
-// arr.shift()
-// arr.slice(2 ,4) // wont work like this instead use:
-// var new_arr=arr.slice(2,4)
+// for(var i=0;i<10;i++){
+//     }
+// console.log(i)   //OUTPUT COMES
 
-var arr =[1,2,3,4,5,6,7,8]
-// var new_arr = arr.filter((element , index) => {
-//     if (element % 2 == 0) return true 
-// })
-// console.log(new_arr)
+// for(let i=0;i<10;i++){
+// }
+// console.log(i) // OUTPUT ERROR
 
-// arr.splice(3,1)
-arr.splice(3,3)
-console.log(arr)
-
-
+if (true){
+    var Name = 'Ferrari'
+        if (true){
+            console.log(Name)
+        }
+    }   
