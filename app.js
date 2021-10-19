4d13462a5a14457c3d3a3c3d352050e4107d3732
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-19 18:54:45 +0530

    Basic od Promises

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-// PROMISES
-
-// setTimeout function
-setTimeout( () =>{
-  console.log('Bhavini')
-}, 5000) //after 5 sec output will be displayed
-
-setTimeout( () =>{
-  console.log('Bhavini')
-}, 5000) 
-console.log('Hii') // first 'hii' is displayed then after 5 sec 'bhavini' is displayed
+var data = fetch('https://jsonplaceholder.typicode.com/todos/10')
+console.log(data)
