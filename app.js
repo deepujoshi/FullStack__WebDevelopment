6df18dc92235500e08196cd5e80eb5a04146750a
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-19 18:35:11 +0530

    setTimeout Function

--- app.js
+++ app.js
@@ -1,34 +1,11 @@
+// PROMISES
 
-// console.log(this)
-// console.log(this.alert('HII'))
+// setTimeout function
+setTimeout( () =>{
+  console.log('Bhavini')
+}, 5000) //after 5 sec output will be displayed
 
-// let car ={
-//     Name:'C Class' ,
-//     Manufacturer: 'Mercedes' ,
-//     print:function (a){
-//         console.log(a)
-//     }
-// }
-// console.log(car)
-
-// let car ={
-//         Name:'C Class' ,
-//         Manufacturer: 'Mercedes' ,
-//         print:function (){
-//             console.log(this)
-//         }
-//     }
-//     car.print()
-
-let car ={
-            Name:'C Class' ,
-            Manufacturer: 'Mercedes' ,
-            print:function (){
-                console.log(this.Name + ' was manufactured by ' + this.Manufacturer)
-                
-                            // ES6--Template String
-                            // Better way to use it
-                console.log(`${this.Name} was created by ${this.Manufacturer}`)
-            }
-        }   
-        car.print()
+setTimeout( () =>{
+  console.log('Bhavini')
+}, 5000) 
+console.log('Hii') // first 'hii' is displayed then after 5 sec 'bhavini' is displayed
