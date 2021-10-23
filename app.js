cf4dbcc21457564e119f6a9bad941e47f5ee0e6d
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-23 14:51:08 +0530

    fetch data (DO NOT USE)

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-// let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => {
-//   let pasrseData = res.json()
-//   console.log(pasrseData)
-  
-// })
+const table = document.querySelector('table')
+console.log(table)
 
-let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => {
-  console.log(data)
+fetch('https://jsonplaceholder.typicode.com/users')
+   .then(res => res.json())
+    .then(data => {
+       data.forEach(user => {
+         let newRow = document.createElement('tr')
+         let dataID = document.createElement('td')
+         let dataIDTextNode = document.createTextNode(user.id)
+         dataID.appendChild(dataIDTextNode)
+         newRow.appendChild(dataID)
+
+         table.appendChild(newRow)
+
+         let newRow1 = document.createElement('tr')
+         let dataID1 = document.createElement('td')
+         let dataIDTextNode1 = document.createTextNode(user.name)
+         dataID1.appendChild(dataIDTextNode1)
+         newRow1.appendChild(dataID1)
+
+         table.appendChild(newRow1)
+
+        //  let newRow = document.createElement('tr')
+        //  let dataID = document.createElement('td')
+        //  let dataIDTextNode = document.createTextNode(user.email)
+        //  dataID.appendChild(dataIDTextNode)
+        //  newRow.appendChild(dataID)
+
+        //  table.appendChild(newRow)
+
+        //  let newRow = document.createElement('tr')
+        //  let dataID = document.createElement('td')
+        //  let dataIDTextNode = document.createTextNode(user.username)
+        //  dataID.appendChild(dataIDTextNode)
+        //  newRow.appendChild(dataID)
+
+        //  table.appendChild(newRow)
+
+
+       })
 })
-
-// console.log(response)
-
-// setTimeout( () => {
-//   console.log(response)
-  
-//   console.log(data)
-// },3000)
