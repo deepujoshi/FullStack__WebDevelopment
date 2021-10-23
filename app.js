9afe59a25a4e7e885bd0a4ce2d5f778de938b412
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-23 15:05:31 +0530

    Fetch Data (USE LIKE THIS)

--- app.js
+++ app.js
@@ -1,42 +1,25 @@
+// -----------------------USE LIKE THIS TO RENDER DATA------------------------------------
+
 const table = document.querySelector('table')
 console.log(table)
+// -----------USING LIKE THIS HELPS TO RESUSE THE DATA---------------
+const createRow = (user) =>{
+  let newRow = document.createElement('tr')
+  let element = `
+    <td>${user.id}</td>
+    <td>${user.name}</td>
+    <td>${user.email}</td>
+    <td>${user.username}</td> `
+        newRow.innerHTML = element
+    return newRow
+}
+
 
 fetch('https://jsonplaceholder.typicode.com/users')
    .then(res => res.json())
     .then(data => {
        data.forEach(user => {
-         let newRow = document.createElement('tr')
-         let dataID = document.createElement('td')
-         let dataIDTextNode = document.createTextNode(user.id)
-         dataID.appendChild(dataIDTextNode)
-         newRow.appendChild(dataID)
-
-         table.appendChild(newRow)
-
-         let newRow1 = document.createElement('tr')
-         let dataID1 = document.createElement('td')
-         let dataIDTextNode1 = document.createTextNode(user.name)
-         dataID1.appendChild(dataIDTextNode1)
-         newRow1.appendChild(dataID1)
-
-         table.appendChild(newRow1)
-
-        //  let newRow = document.createElement('tr')
-        //  let dataID = document.createElement('td')
-        //  let dataIDTextNode = document.createTextNode(user.email)
-        //  dataID.appendChild(dataIDTextNode)
-        //  newRow.appendChild(dataID)
-
-        //  table.appendChild(newRow)
-
-        //  let newRow = document.createElement('tr')
-        //  let dataID = document.createElement('td')
-        //  let dataIDTextNode = document.createTextNode(user.username)
-        //  dataID.appendChild(dataIDTextNode)
-        //  newRow.appendChild(dataID)
-
-        //  table.appendChild(newRow)
-
-
+             table.appendChild(createRow(user))
        })
-})
+ })    
+       
