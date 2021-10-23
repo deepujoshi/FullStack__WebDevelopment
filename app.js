6587122628c4478ab5f971923401bd2e4a83461c
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-23 16:22:53 +0530

    Recipe Api

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-// fetch('https://jsonplaceholder.typicode.com/users')
-//    .then(res => res.json())
-//     .then(data => {
-//        data.forEach(user => {
-//              console.log(data)
-//        })
-//  })    
-       
-const fetchData = async () => {
-const response = await fetch('https://jsonplaceholder.typicode.com/users')
-const data = await response.json()
-console.log(data)
+const ul = document.querySelector('ul')
+const button = document.querySelector('button')
+const getText = document.querySelector('input')
+
+const getData = async (item) => {
+  console.log(item)
+  let url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=Pizza`
+  console.log(url)
+  const res = await fetch(url)
+  const data = await res.json()
+  console.log()
+  data.hits.forEach(recipe => {
+      console.log(recipe)
+  })
 }
-fetchData()
+
+// add event listener to  button
+button.addEventListener('click' , (e) => {
+      console.log(getText.value);
+})
