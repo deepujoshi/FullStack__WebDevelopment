8f3efdc9b4775e25578cbe5545921f97095d1082
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-23 15:29:37 +0530

    Async and Await

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-// -----------------------USE LIKE THIS TO RENDER DATA------------------------------------
-
-const table = document.querySelector('table')
-console.log(table)
-// -----------USING LIKE THIS HELPS TO RESUSE THE DATA---------------
-const createRow = (user) =>{
-  let newRow = document.createElement('tr')
-  let element = `
-    <td>${user.id}</td>
-    <td>${user.name}</td>
-    <td>${user.email}</td>
-    <td>${user.username}</td> `
-        newRow.innerHTML = element
-    return newRow
-}
-
-
-fetch('https://jsonplaceholder.typicode.com/users')
-   .then(res => res.json())
-    .then(data => {
-       data.forEach(user => {
-             table.appendChild(createRow(user))
-       })
- })    
+// fetch('https://jsonplaceholder.typicode.com/users')
+//    .then(res => res.json())
+//     .then(data => {
+//        data.forEach(user => {
+//              console.log(data)
+//        })
+//  })    
        
+const fetchData = async () => {
+const response = await fetch('https://jsonplaceholder.typicode.com/users')
+const data = await response.json()
+console.log(data)
+}
+fetchData()
