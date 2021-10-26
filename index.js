8d90ddb88c972c31446b3f46de42ccd155d92fea
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-26 19:31:43 +0530

    LocalStorage and JSON.stringify & parse method

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const{add,sub,multiple,divide} = require('./app')
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const div = document.querySelector('div')
 
-add()
-sub()
-multiple()
-divide()
+// div.innerHTML = localStorage.getItem('InputValue')
+// button.addEventListener('click' , (req , res) => {
+//   localStorage.setItem('InputValue', input.value)
+//   div.innerHTML = input.value
+// })
+
+// localStorage.setItem('InputValue', "something")
+// console.log(localStorage.getItem("InputValue"))
+
+
+// ------------STRIGNGIFY METHOD ------------------
+const arr = [1, 2, 3, 4,5]
+
+localStorage.setItem('val' , JSON.stringify(arr))
+
+console.log(JSON.parse(localStorage.getItem('val')))
