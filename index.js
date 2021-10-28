c819d6e3ad9f7383de1d51832cdc3b25e8805e2d
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-28 20:12:09 +0530

    read Methods

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const path = require('path')
+const fs = require('fs')
 
-// console.log(path.resolve('./index.js'))
-// console.log(path.relative('../','./temp/app.js'))   //create afolder name temp then moved the app.js  and then excuted this :)
+// -----------------this will print after the consaole which is out of the function---------------
+// fs.readFile('./test.txt' , (error,data) =>{
+// fs.readFile('./test.txt' , 'utf8' , (error,data) =>{
 
-// console.log(path.extname('./app.js'))
-// console.log(path.dirname('./temp/app.js'))
+//   if (error)
+//     console.log(error)
+//   else
+//     console.log(data)
+//     // console.log(data.toString())
+// })
+//------------------use this to make the file inside function to run ---------------------
 
-// console.log(__dirname)
-console.log(path.join(__dirname,'app.js'))
+console.log(fs.readFileSync('./test.txt' , 'utf8'))
+console.log("I'm a random file")
