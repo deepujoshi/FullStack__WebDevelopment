8045222e9ad6002324a3e1d0e6967331aa5d1202
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-28 19:11:04 +0530

    path Methods

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const path = require('path')
 
 // console.log(path.resolve('./index.js'))
-console.log(path.relative('../','./temp/app.js'))   //create afolder name temp then moved the app.js  and then excuted this :)
+// console.log(path.relative('../','./temp/app.js'))   //create afolder name temp then moved the app.js  and then excuted this :)
+
+// console.log(path.extname('./app.js'))
+// console.log(path.dirname('./temp/app.js'))
+
+// console.log(__dirname)
+console.log(path.join(__dirname,'app.js'))
