ed1baa1c26317187fa90eb32796488b8c7d5f241
deepujoshi <deepthijoshi1909@gmail.com>
2021-10-28 20:47:49 +0530

    write method

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 const fs = require('fs')
 
-// -----------------this will print after the consaole which is out of the function---------------
-// fs.readFile('./test.txt' , (error,data) =>{
-// fs.readFile('./test.txt' , 'utf8' , (error,data) =>{
-
-//   if (error)
-//     console.log(error)
-//   else
-//     console.log(data)
-//     // console.log(data.toString())
-// })
-//------------------use this to make the file inside function to run ---------------------
-
-console.log(fs.readFileSync('./test.txt' , 'utf8'))
-console.log("I'm a random file")
+fs.writeFile('./test.txt' ,"HI", (error) =>{
+  if (error)
+    console.log(error)
+  })
