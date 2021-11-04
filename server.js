ea3cb7cb1179a8811268a72442eedd591a56ecab
deepujoshi <deepthijoshi1909@gmail.com>
2021-11-04 11:34:29 +0530

    http module

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const fs = require('fs')
 const path = require('path')
 
 const server = http.createServer((request, response ) => {
-  // console.log(request.method)
-  // console.log(request.headers)
-  // console.log(request.url)
-  // const path = request.url
-  const markup = fs.readFileSync(path.resolve('./index.html'))
-  response.write(markup)
-  response.end()
+  const {url} = request
+  if (url=='/login'){
+    response.write('<h1>LOGIN</h1>')
+    response.end()
+  }
+  if (url=='/signup'){
+    response.write('<h1>SIGNUP</h1>')
+    response.end()
+  }
+
 })
-// console.log(server)
 
 server.listen(3000, () => {
   console.log(`server listening at PORT: ${3000}`)
