36c5e4e0884ba223e98893d57fcbcaf7e24f699d
deepujoshi <deepthijoshi1909@gmail.com>
2021-11-04 12:08:31 +0530

    Express Server

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
+// const http = require('http')
+// const fs = require('fs')
+// const path = require('path')
 
-const server = http.createServer((request, response ) => {
-  const {url} = request
-  if (url=='/login'){
-    response.write('<h1>LOGIN</h1>')
-    response.end()
-  }
-  if (url=='/signup'){
-    response.write('<h1>SIGNUP</h1>')
-    response.end()
-  }
+// const server = http.createServer((request, response ) => {
+//   const {url} = request
+//   if (url=='/login'){
+//     response.write('<h1>LOGIN</h1>')
+//     response.end()
+//   }
+//   if (url=='/signup'){
+//     response.write('<h1>SIGNUP</h1>')
+//     response.end()
+//   }
 
+// })
+
+// server.listen(3000, () => {
+//   console.log(`server listening at PORT: ${3000}`)
+// })
+
+const express = require('express')
+const app =express()
+
+app.get('/login' , (req , res ) => {
+  res.send('this is a LOGIN')
 })
 
-server.listen(3000, () => {
-  console.log(`server listening at PORT: ${3000}`)
+app.get('/signup' , (req , res ) => {
+  res.send('this is a SIGNUP')
+})
+
+app.listen(3000, () => {
+  console.log("Server listening at post 3000")
 })
