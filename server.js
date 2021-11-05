89ce4a302fe6aabdc37f9635e5e3f59b47d572d1
deepujoshi <deepthijoshi1909@gmail.com>
2021-11-05 16:35:46 +0530

    HTTP requests , headers & middleware

--- server.js
+++ server.js
@@ -1,35 +1,22 @@
-// const http = require('http')
-// const fs = require('fs')
-// const path = require('path')
-
-// const server = http.createServer((request, response ) => {
-//   const {url} = request
-//   if (url=='/login'){
-//     response.write('<h1>LOGIN</h1>')
-//     response.end()
-//   }
-//   if (url=='/signup'){
-//     response.write('<h1>SIGNUP</h1>')
-//     response.end()
-//   }
-
-// })
-
-// server.listen(3000, () => {
-//   console.log(`server listening at PORT: ${3000}`)
-// })
-
 const express = require('express')
-const app =express()
-
-app.get('/login' , (req , res ) => {
-  res.send('this is a LOGIN')
+const PORT = 3000
+const app = express()
+app.get('/' , (req, res ) => {
+  console.log(req.headers)
+  res.send("GET")
 })
-
-app.get('/signup' , (req , res ) => {
-  res.send('this is a SIGNUP')
-})
-
-app.listen(3000, () => {
-  console.log("Server listening at post 3000")
+// app.post('/' , (req, res ) => {
+//   res.send("POST")
+// })
+// app.put('/' , (req, res ) => {
+//   res.send("PUT")
+// })
+// app.patch('/' , (req, res ) => {
+//   res.send("PATCH")
+// })
+// app.delete('/' , (req, res ) => {
+//   res.send("DELETE")
+// })
+app.listen(PORT , () =>{
+  console.log(`server running at PORT ${3000}`)
 })
