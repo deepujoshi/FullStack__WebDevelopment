a703bd34f8b301e790a6323cef77d75dade1f81f
deepujoshi <deepthijoshi1909@gmail.com>
2021-11-18 19:46:23 +0530

    basics react app

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Lorem ipsum dolor sit amet. Aut ducimus doloribus nam reprehenderit impedit a unde commodi vel beatae deleniti et culpa facilis. Et maxime sint sed odit aut sint pariatur in tempore quibusdam ut porro libero.
+    <h4>
+      I'm a h4
+    </h4>
+    <h4>
+      I'm a h4
+    </h4>
+    <h4>
+      I'm a h4
+    </h4>
+    <h4>
+      I'm a h4
+    </h4>
+  
     </div>
   );
 }
